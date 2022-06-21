cd8e088f2a246d74dbb384848d9d89c7108bf04e
vladslav008 <vladslav008@mail.ru>
2022-06-21 20:36:42 +0500

    Restricted access to database files.

--- DB/core.php
+++ DB/core.php
@@ -1,7 +1,11 @@
 <?php
 function connect_table($table_name)
 {
-    $conn = new mysqli("localhost", "root", "Ghjvfd!2", $table_name);
+    $hostname = "";
+    $username = "";
+    $password = "";
+
+    $conn = new mysqli($hostname, $username, $password, $table_name);
     if ($conn->connect_error) {
         die("Ошибка: " . $conn->connect_error);
     }
