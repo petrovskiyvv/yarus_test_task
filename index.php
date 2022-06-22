f4b2979407643878711a660053b87472d1d19d90
vladslav008 <vladslav008@mail.ru>
2022-06-23 01:41:50 +0500

    Added textarea auto-expansion.

--- index.php
+++ index.php
@@ -5,9 +5,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Форма добавления</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+        <link rel="stylesheet" href="CSS/style.css">
     </head>
     <body class="text-center vsc-initialized">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+        <script src='https://cdnjs.cloudflare.com/ajax/libs/autosize.js/3.0.15/autosize.min.js'></script>
         <div class="col-md" style="background-color: whitesmoke">
         <div class="container">
         <figure class="text-center">
@@ -34,7 +36,10 @@
             <br>
             <div class="mb-2">
                 <label for="comment" class="form-label">Серийный номер оборудования: </label>
-                <textarea name="comment" class="form-control" rows="2"></textarea>
+                <textarea id='textarea' name="comment" class="form-control" placeholder="Введите SN оборудования" rows="1"></textarea>
+                <script>
+                    autosize(document.querySelectorAll('textarea'));
+                </script>
             </div>
             <div id="commentHelpBlock" class="form-text">
                 При одновременном вводе нескольких номеров, необходимо их разделять переносом строки.
