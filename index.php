e06180a583cb3712270811efa3c58239608ad054
vladslav008 <vladslav008@mail.ru>
2022-06-22 23:04:38 +0500

    Divided the table and the input form into 2 pages.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Оборудование</title>
+        <title>Форма добавления</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     </head>
     <body class="text-center vsc-initialized">
@@ -44,25 +44,6 @@
                 <button class="w-100 btn btn-lg btn-primary" type="submit">Отправить</button>
             </div>
         </form>
-
-        <?php
-        $sql = "SELECT * FROM equipment_list";
-        if($result = $conn->query($sql)){
-            $rowsCount = $result->num_rows; // количество полученных строк
-            echo "<p>Получено объектов: $rowsCount</p>";
-            echo "<table class='table table-striped'><tr><th scope='col'>Id</th><th scope='col'>Модель</th><th scope='col'>Серийный номер</th></tr>";
-            foreach($result as $row){
-                echo "<tr>";
-                echo "<td>" . $row["id"] . "</td>";
-                echo "<td>" . $row["name"] . "</td>";
-                echo "<td>" . $row["serial_num"] . "</td>";
-                echo "</tr>";
-            }
-            echo "</table>";
-            $result->free();
-        }
-        ?>
-
         </div>
         </div>
     </body>
