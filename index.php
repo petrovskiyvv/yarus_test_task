86bbf01514d1632c3da4a391fab81afd3c5396dd
vladslav008 <vladslav008@mail.ru>
2022-06-22 09:03:18 +0500

    Changing the style of the main page.

--- index.php
+++ index.php
@@ -8,14 +8,18 @@
     </head>
     <body>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-        <h3>Форма ввода данных</h3>
+        <div class="col-md" style="background-color: lavenderblush">
+        <figure class="text-center">
+            <p class="h3">Пожалуй, лучшая форма для ввода данных в мире, да что там в мире - в России!</p>
+        </figure>
+        <label for="equipment" class="form-label">Модель оборудования: </label>
         <form action="create.php" method="POST">
             <?php
             include "DB\core.php";
             $conn = connect_table("equipment");
             $sql = "SELECT id, name FROM equipment_template";
             if($result = $conn->query($sql)) {
-                echo "<select name = 'equipment' size='1''>";
+                echo "<select class='form-select' name = 'equipment'>";
                 echo "<option disabled>Выберите оборудование</option>";
                 while ($row = mysqli_fetch_array($result)) {
                     echo "<option value ='" . $row["id"] . "'>" . $row["name"] . "</option>";
@@ -23,11 +27,16 @@
                 echo "</select>";
             }
             ?>
-            <p>Серийный номер оборудования: <br>
-                <label>
-                    <textarea name="comment" maxlength="254"></textarea>
-                </label></p>
-            <input type="submit" value="Отправить">
+            <label for="comment" class="form-label">Серийный номер оборудования: </label>
+            <label>
+                <textarea name="comment" class="form-control"></textarea>
+            </label>
+            <div id="commentHelpBlock" class="form-text">
+                При одновременном вводе нескольких номеров, необходимо их разделять переносом строки.
+            </div>
+            <div class="d-grid gap-2 justify-content-md-end-auto">
+                <button class="btn btn-secondary" type="submit">Отправить</button>
+            </div>
 
         </form>
 
@@ -36,7 +45,7 @@
         if($result = $conn->query($sql)){
             $rowsCount = $result->num_rows; // количество полученных строк
             echo "<p>Получено объектов: $rowsCount</p>";
-            echo "<table><tr><th>Id</th><th>Название</th><th>Серийный номер</th></tr>";
+            echo "<table class='table table-striped table-hover'><tr><th scope='col'>Id</th><th scope='col'>Модель</th><th scope='col'>Серийный номер</th></tr>";
             foreach($result as $row){
                 echo "<tr>";
                 echo "<td>" . $row["id"] . "</td>";
@@ -51,5 +60,6 @@
         }
         $conn->close();
         ?>
+        </div>
     </body>
 </html>
