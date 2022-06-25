684960c716a56648e5d396acbba7c98302c2f23b
vladslav008 <vladslav008@mail.ru>
2022-06-25 05:28:45 +0500

    Changed the SQL configuration files.

--- Config_SQL/equipment_equipment_list.sql
+++ Config_SQL/equipment_equipment_list.sql
@@ -30,17 +30,8 @@ CREATE TABLE `equipment_list` (
   `serial_num` varchar(254) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `serial_num` (`serial_num`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=232 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `equipment_list`
---
-
-LOCK TABLES `equipment_list` WRITE;
-/*!40000 ALTER TABLE `equipment_list` DISABLE KEYS */;
-/*!40000 ALTER TABLE `equipment_list` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -51,4 +42,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-06-25  5:27:13
+-- Dump completed on 2022-06-24 21:55:39
