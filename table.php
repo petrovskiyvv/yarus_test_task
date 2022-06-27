e08606ef9ecedd8e4a1f22955a089772b1453e21
vladslav008 <vladslav008@mail.ru>
2022-06-28 04:57:09 +0500

    Added a <base href=""/> to the root of the project for CSS to work on the page when navigating from another page

--- table.php
+++ table.php
@@ -4,14 +4,15 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Список оборудования</title>
+    <base href="test_task_Yarus/"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="stylesheet" href='CSS/style.css'>
 </head>
 <body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
     <button class="w-100 btn btn-lg btn-primary" onclick="history.back();" type="submit">Вернуться к форме</button>
     <?php
-    include "DB\core.php";
+    include "DB/core.php";
     $conn = connect_table("equipment");
     $sql = "SELECT * FROM equipment_list";
     if($result = $conn->query($sql)) {
